aa129078417642932d7f4246e770ec68898f839e
Maninder Kaur <manikathuria2001@gmail.com>
2022-08-06 15:06:14 +0530

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 const ensureToken = (req,res,next)=>{
 
-    const bearesHeader = req.headers["authorization"];
-    if(typeof bearesHeader !== 'undefined'){
-        const bearer = bearesHeader.split(" ");
+    const bearerHeader = req.headers["authorization"];
+    if(typeof bearerHeader !== 'undefined'){
+        const bearer = bearerHeader.split(" ");
         const bearerToken  = bearer[1];
         req.token = bearerToken;
         next();
